1b2206dd986f229e220d1e85719f7b805b398582
davidbacter01 <davidbacter01@gmail.com>
2024-03-25 18:36:26 +0200

    Add example for promise

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log('hello');
+
+const promise = new Promise((resolve, reject) => {
+    const resolved = false; // schimba in true sau false
+
+    setTimeout(() => {
+        if (resolved) {
+            resolve('Valoare dupa rezolvare');
+        } else {
+            reject('Valoare dupa respingere');
+        }
+    }, 2000);
+});
+
+
+promise.then((resolvedValue) => {
+    console.log('S-a rezolvat cu valoarea: ', resolvedValue);
+}).catch((rejectedValue) => {
+    console.log('S-a respins cu valoarea: ', rejectedValue);
+}).finally(() => {
+    console.log('Se executa mereu!');
+})
+
+console.log('Cod ce se executa simultan cu promisiunea');
